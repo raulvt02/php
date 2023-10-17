342e0f0664a9cb0a709b7bea102ddba6adb07b5d
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2023-10-17 14:42:52 +0200

    Update index.php

--- crud/index.php
+++ crud/index.php
@@ -1,1 +1,18 @@
+<?php
+include 'config.php';
+
+$stmt = $pdo->query('SELECT * FROM jabones');
+$jabones = $stmt->fetchAll();
+?>
+
+<h2>Listado de Jabones</h2>
+<ul>
+<?php foreach ($jabones as $jabon): ?>
+    <li>
+        <?php echo $jabon['nombre']; ?> - $<?php echo $jabon['precio']; ?>
+        <a href="edit.php?id=<?php echo $jabon['id']; ?>">Editar</a>
+        <a href="delete.php?id=<?php echo $jabon['id']; ?>">Eliminar</a>
+    </li>
+<?php endforeach; ?>
+</ul>
 

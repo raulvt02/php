7150d4f6fb1591b64b3f524d0aecd97e30b7f9dd
Pablo <jdommay863@g.educaand.es>
2025-09-22 10:28:31 +0200

    ejercicio 2 realizado

---
+++ actividades/u1/ejercicio2.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+</head>
+<body>
+<?php 
+  $nombre = "Israel";
+  $direccion = "Calle Camas";
+  $numero = 123456789;
+  echo "Mi nombre es $nombre"."<br>La dirección es $direccion <br>Y mi telefono es $numero";
+?>
+</body>
+</html>
+
+

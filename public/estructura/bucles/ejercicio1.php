10f6ee41cf539f18080754c5faad654b3fbab96f
usuario <vera.trashorras.raul@iescamas.es>
2025-09-29 10:08:22 +0200

    ejercicio 1 realizado

---
+++ public/estructura/bucles/ejercicio1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio1</title>
+</head>
+<body>
+    <?php
+        for($i=0;$i<=100;$i++){
+            if($i % 5 == 0){
+                echo("$i" . " es múltiplo de 5.<br>")
+            }
+        }
+        
+    ?>
+    
+</body>
+</html>

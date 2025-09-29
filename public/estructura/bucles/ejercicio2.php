c141aa5fe285ddf09db36c8871a983879b56f729
usuario <vera.trashorras.raul@iescamas.es>
2025-09-29 10:25:57 +0200

    ejercios 2 y 3 realizados

--- public/estructura/bucles/ejercicio2.php
+++ public/estructura/bucles/ejercicio2.php
@@ -3,17 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Ejercicio1</title>
 </head>
 <body>
-    <?php
+   <?php
     $i = 0;
-     do {
-        if ($i % 5 == 0){
+     while ($i <= 100){
+        if ($i % = 0){
         echo "$i es múltiplo de 5.<br><hr>";
         }
         $i++;
-     }while($i <= 100)
+     }
 
  ?>
 </body>
